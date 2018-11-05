0ffe0a626d4ead329f991527e9b56ef71fb26cdd
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-11-05 08:24:10 +0530

    Image attachment.

--- XLRelease/Shashank/Academic Bureau/AcademicBureau Code Review.groovy
+++ XLRelease/Shashank/Academic Bureau/AcademicBureau Code Review.groovy
@@ -1,6 +1,6 @@
 // Exported from:        https://shashank-mbp.xebiaindia.local:5516/#/templates/Folder72a7359cf7bb4b44a36330317d925cf8-Folder4472fd7a7b0f4cf287407483eef74347-Releaseb43585cf80de49bf9734298f9243362e/releasefile
 // XL Release version:   8.2.0
-// Date created:         Mon Nov 05 08:19:52 IST 2018
+// Date created:         Mon Nov 05 08:24:07 IST 2018
 
 xlr {
   template('AcademicBureau Code Review') {
@@ -11,8 +11,12 @@ xlr {
     phases {
       phase('New Phase') {
         tasks {
-          notification('Notification Task') {
-            
+          notification('Notification') {
+            attachments {
+              attachment("sql:Attachment3b842e89c06d4a39a66993d17d7b25c3") {
+                filename "attachments/Attachment3b842e89c06d4a39a66993d17d7b25c3-Ubuntu_XLD_Server__Running_.png"
+              }
+            }
           }
           custom('Code Quality Gate') {
             script {
